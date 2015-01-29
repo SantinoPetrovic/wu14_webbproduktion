a86c511c5f957028f71273c43931cd6f32780c1f
SantinoPetrovic <santinopetrovic@gmail.com>
2015-01-29 14:43:01 +0100

    AJAX call for formular.

--- js/main.js
+++ js/main.js
@@ -14,4 +14,27 @@ $(function(){
 	$(".pageCancel").click(function(){
 		$(".pageFormular").slideUp(300);
 	});
+
+	$(".submitButton").submit(function(){
+		var insertContent = {
+			":title" : $(this).find("#titleValue").val(),
+			":content" : $(this).find("#contentValue").val()
+		};
+
+		$.ajax({
+			url: "php/store_content.php",
+			type: "post",
+			dataType: "json",
+			data: {
+
+			},
+			success: function(data){
+				console.log("store_content success: ", data);
+			},
+			error: function(data){
+				console.log("store_content error: ", data);
+			}
+		});
+		return false;
+	});
 });
