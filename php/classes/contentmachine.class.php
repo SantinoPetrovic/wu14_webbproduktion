8f55fecfa0ac65586d7123282ab55132efdad7e8
Santino Petrovic <santinopetrovic@gmail.com>
2015-01-30 15:25:02 +0100

    Trying to send the data to DB but still dosen't work.

--- php/classes/contentmachine.class.php
+++ php/classes/contentmachine.class.php
@@ -6,11 +6,14 @@ class ContentMachine extends PDOHelper {
     protected $user_info = array("user_id" => 1);
 
     public function saveContent($insertContent) {
-        $insertContent[":user_id"] = $this->user_info["user_id"];
+        // $insertContent[":user_id"] = $this->user_info["user_id"];
         // $page_path = $insertContent[":path"];
         // unset($insertContent[":path"]);
         // $menu_data = $insertContent["menuData"];
         // unset($insertContent["menuData"]);
         $pageSQL = "INSERT INTO pages (title, content) VALUES (:title, :content)";
+        $this->query($insertContent);
+        return true;
     }
+
 }
