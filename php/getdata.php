cae3ab6898bf2853c352c1d029fcda1e3e6495c0
Santino Petrovic <santinopetrovic@gmail.com>
2015-02-10 11:12:17 +0100

    Changed password.

--- php/getdata.php
+++ php/getdata.php
@@ -1,6 +1,6 @@
 <?php
 include_once("autoloader.php");
-$cq = New ContentMachine("127.0.0.1","kebab","root","188830");
+$cq = New ContentMachine("127.0.0.1","kebab","root","mysql");
 if (isset($_REQUEST["catId"])) {
 	echo(json_encode($cq->getContentByCat($_REQUEST["catId"])));
 } else {
