8a1b03643681e70b15ae399dea4b395f35e7048f
santino <santinopetrovicgmail.com>
2015-02-20 23:48:36 +0100

    Changed password.

--- php/deletedata.php
+++ php/deletedata.php
@@ -1,6 +1,6 @@
 <?php
 include_once("autoloader.php");
-$cq = New ContentMachine("127.0.0.1","kebab","root","188830");
+$cq = New ContentMachine("127.0.0.1","kebab","root","mysql");
 if (isset($_REQUEST["deletePage"])) {
     echo(json_encode($cq->deletePageByPag($_REQUEST["deletePage"])));
 }
--- php/getdataCategory.php
+++ php/getdataCategory.php
@@ -1,6 +1,6 @@
 <?php
 include_once("autoloader.php");
-$cq = New ContentMachine("127.0.0.1","kebab","root","188830");
+$cq = New ContentMachine("127.0.0.1","kebab","root","mysql");
 if (isset($_REQUEST["catID"])) {
 
     echo(json_encode($cq->editCategoryByCat($_REQUEST["catID"])));
